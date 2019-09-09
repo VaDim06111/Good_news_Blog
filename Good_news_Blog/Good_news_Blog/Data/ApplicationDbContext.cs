c51567e5f641be8fbfaea1ba8f684b6595173800
Vadim Bondarovich <worldvb2000@gmail.com>
2019-09-10 00:17:17 +0300

    first commit

---
+++ Good_news_Blog/Good_news_Blog/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Good_news_Blog.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<News> News { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { }
+    }
+}
---
+++ Good_news_Blog/Good_news_Blog/Data/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Good_news_Blog.Data
+{
+    public class User
+    {
+        public Guid Id { get; set; }        
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+    }
+}
