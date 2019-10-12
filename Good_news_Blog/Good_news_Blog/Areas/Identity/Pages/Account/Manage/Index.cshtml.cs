686c389ccb81e573ab6f09c5ff40887c82154510
Vadim Bondarovich <worldvb2000@gmail.com>
2019-10-12 15:54:38 +0300

    Fix text on message

--- Good_news_Blog/Good_news_Blog/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ Good_news_Blog/Good_news_Blog/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -56,7 +56,7 @@ namespace Good_news_Blog.Areas.Identity.Pages.Account.Manage
             var user = await _userManager.GetUserAsync(User);
             if (user == null)
             {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+                return NotFound($"Невозможно загрузить пользователя с идентификатором '{_userManager.GetUserId(User)}'.");
             }
 
             var userName = await _userManager.GetUserNameAsync(user);
@@ -86,7 +86,7 @@ namespace Good_news_Blog.Areas.Identity.Pages.Account.Manage
             var user = await _userManager.GetUserAsync(User);
             if (user == null)
             {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+                return NotFound($"Невозможно загрузить пользователя с идентификатором '{_userManager.GetUserId(User)}'.");
             }
 
             var userName = await _userManager.GetUserNameAsync(user);
@@ -96,7 +96,7 @@ namespace Good_news_Blog.Areas.Identity.Pages.Account.Manage
                 if (!setUserNameResult.Succeeded)
                 {
                     var userId = await _userManager.GetUserIdAsync(user);
-                    throw new InvalidOperationException($"Unexpected error occurred setting email for user with ID '{userId}'.");
+                    throw new InvalidOperationException($"Произошла непредвиденная ошибка при настройке имени пользователя для пользователя с идентификатором '{userId}'.");
                 }
             }
 
@@ -107,7 +107,7 @@ namespace Good_news_Blog.Areas.Identity.Pages.Account.Manage
                 if (!setEmailResult.Succeeded)
                 {
                     var userId = await _userManager.GetUserIdAsync(user);
-                    throw new InvalidOperationException($"Unexpected error occurred setting email for user with ID '{userId}'.");
+                    throw new InvalidOperationException($"Произошла непредвиденная ошибка при настройке электронной почты для пользователя с идентификатором '{userId}'.");
                 }
             }
 
@@ -118,12 +118,12 @@ namespace Good_news_Blog.Areas.Identity.Pages.Account.Manage
                 if (!setPhoneResult.Succeeded)
                 {
                     var userId = await _userManager.GetUserIdAsync(user);
-                    throw new InvalidOperationException($"Unexpected error occurred setting phone number for user with ID '{userId}'.");
+                    throw new InvalidOperationException($"Произошла непредвиденная ошибка при настройке номера телефона для пользователя с идентификатором '{userId}'.");
                 }
             }
 
             await _signInManager.RefreshSignInAsync(user);
-            StatusMessage = "Your profile has been updated";
+            StatusMessage = "Ваш профиль был обновлен";
             return RedirectToPage();
         }
 
@@ -137,7 +137,7 @@ namespace Good_news_Blog.Areas.Identity.Pages.Account.Manage
             var user = await _userManager.GetUserAsync(User);
             if (user == null)
             {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+                return NotFound($"Невозможно загрузить пользователя с идентификатором '{_userManager.GetUserId(User)}'.");
             }
 
 
@@ -152,9 +152,9 @@ namespace Good_news_Blog.Areas.Identity.Pages.Account.Manage
             await _emailSender.SendEmailAsync(
                 email,
                 "Confirm your email",
-                $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                $"Пожалуйста, подтвердите свой аккаунт по <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>нажатию здесь</a>.");
 
-            StatusMessage = "Verification email sent. Please check your email.";
+            StatusMessage = "Письмо с подтверждением отправлено. Пожалуйста, проверьте вашу электронную почту.";
             return RedirectToPage();
         }
     }
